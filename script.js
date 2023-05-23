6b963610b01639acb98f7cdf7bfcb5a95b4cff95
Loïc Hernandez <loichernandez@mini-de-loic.home>
2023-05-23 14:28:28 +0200

    1st finish before compilation

--- script.js
+++ script.js
@@ -54,7 +54,16 @@ for (let i = 0; i < menuElement.length; i++) {
 const projects = document.querySelector('#list-projects')
 
 const listOfProjects = [
-    ['hypnos', 'https://hypnos-three.vercel.app/'], ['watowatch', "https://tv-shows-data.vercel.app/"]
+    [
+        'hypnos', 
+        'https://hypnos-three.vercel.app/', 
+        "Projet Titre Professionnel Niv 5"
+    ], 
+    [
+        'watowatch', 
+        "https://tv-shows-data.vercel.app/", 
+        "Formation Udemy"
+    ]
 ]
 
 for (let i = 0; i < listOfProjects.length; i++) {
@@ -62,10 +71,17 @@ for (let i = 0; i < listOfProjects.length; i++) {
     let a = document.createElement('a')
     let img = document.createElement('img')
     let div = document.createElement('div')
+    let span = document.createElement('span')
 
     div.style.display = "flex"
     div.style.alignItems = "center"
     div.style.gap = "10px"
+
+    span.innerText = listOfProjects[i][2]
+    span.style.fontStyle = "oblique"
+    span.style.fontWeight = "normal"
+    span.style.marginLeft = "15px"
+    span.style.color = "rgb(255, 148, 110)"
 
     img.setAttribute('src', './assets/images/' + listOfProjects[i][0] + '/logo.png')
     img.style.width = "80px"
@@ -73,12 +89,14 @@ for (let i = 0; i < listOfProjects.length; i++) {
     img.style.borderRadius = "20px"
 
     a.setAttribute('href', listOfProjects[i][1])
+    a.setAttribute('target', '_blank')
     a.classList.add("link-project")
 
     div.classList.add("label-project")
 
     a.append(listOfProjects[i][0].toUpperCase())
     p.append(a)
+    p.append(span)
     div.appendChild(img)
     div.appendChild(p)
     projects.appendChild(div)
@@ -97,30 +115,144 @@ const trainingInfo = [
     ],
     [
         '2002',
-        'Action & communication commerciale',
+        'Action & Communication Commerciale',
         'Baccalauréat',
         'Lycée Marcelin Berthelot - Toulouse'
     ]
 ]
 
-for (let i = 0; i < trainingInfo.length ; i++){
+for (let i = 0; i < trainingInfo.length; i++) {
     const divTraining = document.createElement('div')
+    divTraining.style.display = "flex"
+    divTraining.style.marginBottom = "10px"
+
+    const divDateTraining = document.createElement('div')
+
+    const divTitleTraining = document.createElement('div')
+    const divCompanyTraining = document.createElement('div')
+
     const h2TrainingDate = document.createElement('h2')
     const h2LabelTraining = document.createElement('h2')
-    
+    const pCompanyTraining = document.createElement('p')
+
     h2TrainingDate.append(trainingInfo[i][0])
-    h2TrainingDate.style.marginRight = '20px'
 
     h2LabelTraining.append(trainingInfo[i][1])
-    
-    divTraining.appendChild(h2TrainingDate)
-    divTraining.appendChild(h2LabelTraining)
-    divTraining.style.display = 'flex'
-    divTraining.style.alignItems = 'center'
-    divTraining.style.gap = "10px"
-    divTraining.style.padding = "10px"
-    
+    h2LabelTraining.style.color = "rgb(255, 148, 110)"
+
+    pCompanyTraining.append(trainingInfo[i][2] + " - " + trainingInfo[i][3])
+
+    divDateTraining.appendChild(h2TrainingDate)
+    divTitleTraining.appendChild(h2LabelTraining)
+    divTitleTraining.appendChild(pCompanyTraining)
+    // divTitleTraining.style.display = 'flex'
+    // divTitleTraining.style.alignItems = 'center'
+
+    divTraining.appendChild(divDateTraining)
+    divTraining.appendChild(divTitleTraining)
     training.appendChild(divTraining)
+}
+
+////////////////////////////// XP
+
+const xp = document.querySelector('#xp')
+
+const xpList = [
+    [
+        "2023", 
+        "Tikilearning", 
+        "Stage Développeur FullStack", 
+        [
+            ["python", "Création de modules Odoo"],
+            ["odoo","Création de modules avec Python"], 
+            ["vuejs", "Création de l'UI, Mobile First & Responsive"], 
+            ["tailwind", "Création de l'UI, Mobile First & Responsive"], 
+            ["jira", "Méthode Agile, US, Tickets, Sprint"], 
+            ["docker", "Utilisation de conteneur"]
+        ]
+    ],
+    [
+        "2022",
+        "Hypnos",
+        "Projet Titre Professionnel",
+        [
+            ["react", "Création de l'UI, Mobile First & Responsive"],
+            ["php", "Interface Administrateur, API"],
+            ["git", "Gestion du versionning"],
+            ["bootstrap", "Création de l'UI, Mobile First & Responsive"],
+            ["mysql", "Gestion C.R.U.D."]
+        ]
+    ],
+]
+
+for (let i = 0; i < xpList.length; i++) {
+
+    const xpDate = document.createElement('h2')
+    const xpCompany = document.createElement('h2')
+    const xpProfil = document.createElement('h2')
+    const xpImgDivContainer = document.createElement('div')
+    const divTextXp = document.createElement('div')
+    const textXp = document.createElement('p')
+
+
+    xpImgDivContainer.style.display = "flex"
+    xpImgDivContainer.style.gap = "10px"
+
+    divTextXp.style.display = "flex"
+    divTextXp.style.alignItems = "center"
+    divTextXp.style.height = "50px"
+
+    xpDate.append(xpList[i][0])
+    xpCompany.append(xpList[i][1])
+    xpCompany.style.color = "rgb(255, 148, 110)"
+    xpProfil.append(xpList[i][2])
+
+    const div = document.createElement('div')
+    div.style.display = "flex"
+    // div.style.width = "100%"
+    div.appendChild(xpDate)
+    div.appendChild(xpCompany)
+    div.appendChild(xpProfil)
+
+    xp.appendChild(div)
+
+    for (let y = 0; y < xpList[i][3].length; y++) {
+
+        let imgDivxp = document.createElement('div')
+        let imgxp = document.createElement('img')
+        let language = xpList[i][3][y][0].toUpperCase()
+        let languageDescription = xpList[i][3][y][1]
+
+        imgDivxp.style.display = "flex"
+        imgDivxp.style.height = "50px"
+        imgDivxp.style.marginTop = "10px"
+        imgDivxp.style.borderRadius = "10px"
+        imgDivxp.style.alignItems = "center"
+        imgDivxp.style.background = "rgba(255,255,255,1)"
+
+
+        imgSrc = `./assets/icons/${xpList[i][3][y][0]}.png`
+        imgxp.setAttribute('src', imgSrc)
+        imgxp.style.filter = 'grayscale(1)'
+        imgxp.style.margin = "5px"
+        imgxp.style.height = "80%"
+        imgxp.style.cursor = "pointer"
+        imgxp.addEventListener('mouseenter', () => {
+            imgxp.style.filter = 'grayscale(0)'
+        })
+        imgxp.addEventListener('mouseleave', () => {
+            imgxp.style.filter = 'grayscale(1)'
+        })
+        imgxp.addEventListener('click', () => {
+            textXp.innerText = language + " : " + languageDescription
+        })
+
+        divTextXp.appendChild(textXp)
+        imgDivxp.append(imgxp)
+        xpImgDivContainer.appendChild(imgDivxp)
+    }
+    xp.appendChild(xpImgDivContainer)
+    xp.appendChild(divTextXp)
 }
 
 ////////////////////////////// ALL
