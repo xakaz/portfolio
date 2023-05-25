39948f79f330e6a65164727bebd3b5823c13020c
Loïc Hernandez <loichernandez@mini-de-loic.home>
2023-05-25 09:56:51 +0200

    do not display technos on phone devices if landscape

--- script.js
+++ script.js
@@ -245,13 +245,13 @@ for (let i = 0; i < xpList.length; i++) {
 
     xp.appendChild(xpImgDivContainer)
 
-    if (navigator.userAgent.match(/Android/i)
+    if ((navigator.userAgent.match(/Android/i)
         || navigator.userAgent.match(/webOS/i)
         || navigator.userAgent.match(/iPhone/i)
-        || navigator.userAgent.match(/iPad/i)
         || navigator.userAgent.match(/iPod/i)
         || navigator.userAgent.match(/BlackBerry/i)
-        || navigator.userAgent.match(/Windows Phone/i)) { 
+        || navigator.userAgent.match(/Windows Phone/i)) 
+        && window.innerWidth > window.innerHeight) { 
             xpImgDivContainer.style.display = "none"
         }
     xp.appendChild(divTextXp)
